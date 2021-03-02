3d09b6a69b296f8e45a151061238ddda856e400a
Filipe Abner <filipeabnersantos@gmail.com>
2021-03-01 21:34:47 -0300

    Exercicio 1 resolvido

--- src/App.java
+++ src/App.java
@@ -2,8 +2,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         try {
-            int i = 10;
-            System.out.println("Fatorial do número ("+i+") é: "+calcularFatorial(i));  
+            int i = 100;
+            // System.out.println("Fatorial do número ("+i+") é: "+calcularFatorial(i));  
+            System.out.println("Número ("+i+") em binario é: "); 
+            naturalToBinary(i); 
+            System.out.println();
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
@@ -17,5 +20,12 @@ public class App {
             return 1;
         }
        return (n * calcularFatorial(n - 1));
+    }
+    // 1.Impressão de um número natural em base binária.
+    public static void naturalToBinary(int n) {
+        if(n / 2 != 0){
+            naturalToBinary(n/2);
+        }
+        System.out.print(n%2);
     }
 }
